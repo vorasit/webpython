6b7134181406aa62d66ea1efe46e7987179ab1c3
Pinzaa <vorasit.pin@gmail.com>
2021-04-30 02:48:39 +0700

    web
    web

--- djangobasic/blogs/views.py
+++ djangobasic/blogs/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(request):
+
     return HttpResponse("<h1> HELLO WORLD </h1>")
 
 def web(request):
-    return render(request,'index.html') # web/index.html  in templates
+
+    return render(request,'index.html',{'name':'บทความทำไงให้หล่อ',
+    'author':'pinzaa'}) # web/index.html  in templates
