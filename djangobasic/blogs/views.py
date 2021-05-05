08421b1eb4e187593a7db92705a2d5c618ed8efb
Pinzaa <vorasit.pin@gmail.com>
2021-05-06 04:47:46 +0700

    if statement

--- djangobasic/blogs/views.py
+++ djangobasic/blogs/views.py
@@ -10,10 +10,12 @@ def web(request):
     age = 36
     txt = "My name is John, and I am {}"
     text1 = txt.format(age)
+    rateing = 4
     return render(request,'index.html',
     {
         'name':'บทความทำไงให้หล่อ',
         'author':'pinzaa',
         'tags':tags,
-        'text':text1
+        'text':text1,
+        'rateing':rateing
     }) # web/index.html  in templates
